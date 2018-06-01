f512cdec67ed47f6512031d044625ef6013853bc
Brent Bauman <brentbauman86@gmail.com>
2018-06-01 21:12:31 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,9 +9,8 @@ class Triangle
   end
 
   def kind
-    s1 * s2 * s3 == 0 ||
 
-    if s1 + s2 <= s3 || s2 + s3 <= s1 || s3 + s1 <= s2
+    if s1 * s2 * s3 == 0 || s1 + s2 <= s3 || s2 + s3 <= s1 || s3 + s1 <= s2
       begin
         raise TriangleError
         # puts error.message
