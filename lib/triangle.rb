232f3e44bc6f5096b1dc3d641c3312b470bb756a
Brent Bauman <brentbauman86@gmail.com>
2018-06-01 20:10:17 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,17 +2,19 @@ class Triangle
 
   attr_accessor :equilateral, :isosceles, :scalene
 
-  def initialize()
-    
+  def initialize(2, 2, 2)
+
   end
 
   def kind
-
+    2 = :equilateral
+    2 = :isosceles
+    2 = :scalene
   end
 
   class TriangleError < StandardError
     def message
-      
+
     end
   end
 
